07437ee70b14170a6c84244a40142158f945713b
Pablo Ezequiel Caero <pablo_ieme@hotmail.com>
2022-12-29 22:31:26 -0300

    Modificacion en Nav

--- src/components/Navegacion.js
+++ src/components/Navegacion.js
@@ -1,5 +1,5 @@
 //TREE SHAKING 
-//import {Link} from 'react-router-dom' /*PAQUETE QUE INSTALAMOS - LINK SERIA LO EQUIVALENTE A HREF */
+import {Link} from 'react-router-dom' /*PAQUETE QUE INSTALAMOS - LINK SERIA LO EQUIVALENTE A HREF */
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -13,11 +13,14 @@ export default function Navegacion(){
         <Container>
           <Navbar.Brand href="/">[PÁGINA WEB]</Navbar.Brand>
           <Nav className="me-auto">
-           
-           <Nav.Link href="/">Home</Nav.Link> {/*PARA QUE RESPETE LA RAIZ SOLO ES LA BARRITA EN LA HOME*/}
-           <Nav.Link href="/nosotros">Nosotros</Nav.Link>      
-           <Nav.Link href="/juli">Mensajito oculto #9</Nav.Link>
-           <Nav.Link href="/agregartarjeta">Agregar Tarjeta</Nav.Link>
+           &nbsp
+           <Link to="/">Home</Link> {/*PARA QUE RESPETE LA RAIZ SOLO ES LA BARRITA EN LA HOME*/}
+           &nbsp
+           <Link to="/nosotros">Nosotros</Link>      
+           &nbsp
+           <Link to="/juli">Mensajito oculto #9</Link>
+           &nbsp
+           <Link to="/agregartarjeta">Agregar Tarjeta</Link>
 
           </Nav>
         </Container>
