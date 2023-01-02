141dffd8e7375bcab210cbf5bedc988d60b04a8c
Pablo Ezequiel Caero <pablo_ieme@hotmail.com>
2023-01-02 16:35:13 -0300

    Desarrollo de Insertar (Incompleto)

--- src/pages/ABMTarjetas.js
+++ src/pages/ABMTarjetas.js
@@ -1,17 +1,43 @@
 import tarjetas from '../data/tarjetas.json' //IMPORTO LA DATA
-import { Table, Button, Container/*, Modal, ModalBody, ModalHeader, FormGroup, ModalFooter */} from 'react-bootstrap';
-//import { useState } from "react";
+import {Table, Button, Container, Modal, ModalHeader, FormGroup, ModalFooter, ModalBody, FormCheck} from 'react-bootstrap';
+import { useState } from "react";
 
-export default function ABM_Tarjetas() {
-    
+
+export default function ABMTarjetas() {
+
+  
+
+  //PARA MANEJO DE MODAL INSERTAR
+  const [showInsertar, setShowInsertar] = useState(false);
+  const handleCloseInsertar = () => setShowInsertar(false);
+  const handleShowInsertar = () => setShowInsertar(true);
+
+  //PARA INSERTAR
+  function insertar(){
+   tarjetas.push({
+      "index": "16",
+      "titulo": "[RECETA 16]",
+      "imagen": "images/pastas.jpg",
+      "precio": "[TIEMPO Y DIFICULTAD]",
+      "envio_gratis": true
+      
+    })
+
+    setShowInsertar(false);
+  }
+
+  
+  
 
     return (
       <>
+
+
      
       <Container>
 
         <br />
-        <Button variant ="primary">Insertar Nuevo Elemento</Button>
+        <Button variant ="primary" onClick={handleShowInsertar}>Insertar Nuevo Elemento</Button>
         <br /><br />
 
         <Table>
@@ -56,7 +82,91 @@ export default function ABM_Tarjetas() {
         </Table>
       </Container>
 
+    <Modal show={showInsertar} onHide={handleCloseInsertar}>
+          {/*MODAL PARA ALTA*/}
+         <ModalHeader>
+           <div><h3>Insertar Registro</h3></div>
+         </ModalHeader>
 
+         <ModalBody>
+            <FormGroup>
+              <label>
+                ID:
+              </label>
+
+              <input
+                id = "txtIndex"
+                className="form-control"
+                readOnly
+                type="text"
+                value={tarjetas.length+1}
+              
+              />        
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Título:
+              </label>
+
+              <input
+                className="form-control"
+                id = "txtTitulo"
+                type="text"
+              
+              
+              />        
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Precio: $
+              </label>
+
+              <input
+                className="form-control"
+                id = "txtPrecio"
+                type="text"
+              
+              
+              />        
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                URL Imágen:
+              </label>
+
+              <input
+                className="form-control"
+                name = "txtTitulo"
+                type="text"
+                
+              />        
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Envío Gratis:
+              </label>
+
+              <FormCheck 
+                type="switch"
+                name="chkEnvio"  
+                    
+               />   
+            </FormGroup>
+
+         </ModalBody>
+
+        
+            <ModalFooter>
+              <Button variant="primary" oncClick={insertar}>Insertar</Button>
+              <Button variant="danger" onClick={handleCloseInsertar}>Cancelar</Button>
+            </ModalFooter>
+
+
+    </Modal>
 
      </>
     );
